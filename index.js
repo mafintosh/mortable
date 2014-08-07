3a0f9114c6e86fee2db76d88dbf89aa3d8c21bd0
Mathias Buus <mathiasbuus@gmail.com>
2014-08-07 23:13:26 +0200

    another iteration

--- index.js
+++ index.js
@@ -1,18 +1,15 @@
+var cuid = require('cuid')
 var protobufs = require('protocol-buffers-stream')
 var once = require('once')
-var fs = require('fs')
-var cuid = require('cuid')
-var events = require('events')
 var util = require('util')
+var fs = require('fs')
 
-var createStream = protobufs(fs.readFileSync(require.resolve('./schema.proto')))
-
-// op enum
+var createStream = protobufs(fs.readFileSync(require.resolve('./mortable.proto')))
 
 var PUSH = 1
 var PULL = 2
 var HEARTBEAT = 3
-var FIN = 4
+var QUIT = 4
 
 var toArray = function(map) {
   return Object.keys(map).map(function(key) {
@@ -25,175 +22,205 @@ var Peer = function(id) {
   this.seq = 0
   this.heartbeat = 0
   this.changes = []
-
-  this._sets = {}
+  this.values = {}
 }
 
-Peer.prototype.push = function(key, val) {
-  var set = this._sets[key]
-  if (!set) set = this._sets[key] = []
-  if (set.indexOf(key) === -1) set.push(val)
+Peer.prototype.push = function(key, value) {
+  var vals = this.values[key]
+  if (!vals) vals = this.values[key] = []
+  if (vals.indexOf(value) === -1) vals.push(value)
 }
 
-Peer.prototype.pull = function(key, val) {
-  var set = this._sets[key] || []
-  var i = set.indexOf(val)
+Peer.prototype.pull = function(key, value) {
+  var vals = this.values[key]
+  var i = vals ? vals.indexOf(value) : -1
   if (i === -1) return
+  if (vals.length === 1) delete this.values[key]
+  else vals.splice(i, 1)
+}
 
-  if (set.length === 1) delete this._sets[key]
-  else set.splice(i, 1)
+Peer.prototype.has = function(key) {
+  return !!this.values[key]
 }
 
 Peer.prototype.list = function(key) {
-  return key === undefined ? Object.keys(this._sets) : (this._sets[key] || null)
+  return key === undefined ? Object.keys(this.values) : this.values[key]
 }
 
-var Mortable = function(id) {
-  if (!(this instanceof Mortable)) return new Mortable(id)
-  events.EventEmitter.call(this)
+Peer.prototype.update = function(change, table) {
+  if (change.seq < this.seq) return false
+  this.seq = change.seq
 
-  this.id = id || cuid()
-  this.peers = {}
-  this.seq = 0
-  this.heartbeat = 0
-  this.streams = []
-  this.changes = []
-
-  var self = this
-  var update = function() {
-    self.heartbeat++
+  // do not record heartbeat messages
+  if (change.op === HEARTBEAT) {
+    this.heartbeat = table._tick+1
+    return true
   }
-
-  this.interval = setInterval(update, 10000)
-  if (this.interval.unref) this.interval.unref()
-}
-
-util.inherits(Mortable, events.EventEmitter)
-
-Mortable.prototype.destroy = function() {
-  clearInterval(this.interval)
-  this._change(FIN, null, null)
-  for (var i = 0; i < this.streams.length; i++) {
-    this.streams[i].finalize()
-  }
-}
-
-Mortable.prototype.push = function(key, val) {
-  this._change(PUSH, key, val)
-}
-
-Mortable.prototype.pull = function(key, val) {
-  this._change(PULL, key, val)
-}
-
-Mortable.prototype.list = function(key) {
-  var list = Object.keys(this.peers)
-  var result = {}
-  for (var i = 0; i < list.length; i++) {
-    var p = this.peers[list[i]]
-    if (!p || p.heartbeat <= this.heartbeat) continue
-    var l = p.list(key)
-    if (!l) continue
-    for (var j = 0; j < l.length; j++) result[l[j]] = true
-  }
-  return Object.keys(result)
-}
-
-Mortable.prototype._change = function(op, key, val) {
-  this._update({op:op, seq:++this.seq, from:this.id, key:key, value:val}, null)
-}
-
-Mortable.prototype._update = function(change, stream) {
-  var p = this.peers[change.from]
-  if (p === null) return // was permanently deleted
-  if (!p) p = this.peers[change.from] = new Peer(change.from)
-
-  if (p.seq >= change.seq) return
-  p.seq = change.seq
 
   switch (change.op) {
     case PUSH:
-    p.push(change.key, change.value)
-    this.emit('push', change.key, change.value, change.from)
-    this.emit('update', change.key, change.value)
-    this.changes.push(change)
+    this.push(change.key, change.value)
     break
 
     case PULL:
-    p.pull(change.key, change.value)
-    this.emit('pull', change.key, change.value, change.from)
-    this.emit('update', change.key, change.value)
-    this.changes.push(change)
+    this.pull(change.key, change.value)
     break
 
-    case HEARTBEAT:
-    p.heartbeat = this.heartbeat+2
-    break
-
-    case FIN:
-    this.peers[p.id] = null
-    this.changes = this.changes.filter(function(change) {
-      return change.from !== p.id
-    })
+    case QUIT:
+    this.heartbeat = 0
+    this.changes = []
     break
 
     default:
-    return
+    return false
   }
 
-  for (var i = 0; i < this.streams.length; i++) {
-    if (this.streams[i] !== stream) this.streams[i].change(change)
+  this.changes.push(change)
+
+  return true
+}
+
+var Mortable = function(opts) {
+  if (!(this instanceof Mortable)) return new Mortable(opts)
+  if (!opts) opts = {}
+
+  this.ttl = opts.ttl || 10000
+  this.id = opts.id || cuid()
+
+  var self = this
+  var tick = function() {
+    self._tick++
+    self.heartbeat()
+  }
+
+  this._tick = 1
+  this._peers = {}
+  this._local = this._peers[this.id] = new Peer(this.id)
+  this._interval = setInterval(tick, (this.ttl / 2) | 0)
+  this._streams = []
+
+  if (this._interval.unref) this._interval.unref()
+}
+
+Mortable.prototype.has = function(key) {
+  var ids = Object.keys(this._peers)
+
+  for (var i = 0; i < ids.length; i++) {
+    if (this._peers[ids[i]].has(key)) return true
+  }
+
+  return false
+}
+
+Mortable.prototype.list = function(key) {
+  var set = arguments.length === 0 ? {} : null
+  var ids = Object.keys(this._peers)
+
+  for (var i = 0; i < ids.length; i++) {
+    var p = this._peers[ids[i]]
+    if (p !== this._local && p.heartbeat < this._tick) continue
+
+    var list = p.list(key)
+
+    if (!list) continue
+    if (!set) set = {}
+
+    for (var j = 0; j < list.length; j++) set[list[j]] = true
+  }
+
+  return set && Object.keys(set)
+}
+
+Mortable.prototype.destroy = function() {
+  clearInterval(this._interval)
+
+  this._update(null, {
+    op: QUIT,
+    peer: this.id,
+    seq: this._local.seq+1
+  })
+
+  for (var i = 0; i < this._streams.length; i++) {
+    this._streams[i].finalize()
   }
 }
 
-Mortable.prototype.createStream = function() {
-  var s = createStream()
-  var self = this
+Mortable.prototype.push = function(key, value) {
+  this._update(null, {
+    op: PUSH,
+    peer: this.id,
+    seq: this._local.seq+1,
+    key: key,
+    value: value
+  })
+}
 
-  var heartbeat = function() {
-    self._change(HEARTBEAT, null, null)
+Mortable.prototype.pull = function(key, value) {
+  this._update(null, {
+    op: PULL,
+    peer: this.id,
+    seq: this._local.seq+1,
+    key: key,
+    value: value
+  })
+}
+
+Mortable.prototype.heartbeat = function() {
+  this._update(null, {
+    op: HEARTBEAT,
+    peer: this.id,
+    seq: this._local.seq+1
+  })
+}
+
+Mortable.prototype._update = function(from, change) {
+  var peer = this._peers[change.peer] || (this._peers[change.peer] = new Peer(change.peer))
+  if (!peer.update(change, this)) return
+
+  for (var i = 0; i < this._streams.length; i++) {
+    if (this._streams[i] !== from) this._streams[i].change(change)
   }
+}
 
-  s.once('handshake', function(handshake) {
-    var interval = setInterval(heartbeat, 5000)
-    if (interval.unref) interval.unref()
+var addSeq = function(result, peer) {
+  result[peer.id] = peer.seq
+  return result
+}
 
-    var cleanup = once(function() {
-      clearInterval(interval)
-      self.streams.splice(self.streams.indexOf(s), 1)
-    })
+Mortable.prototype.createStream = function() {
+  var self = this
+  var s = createStream()
 
-    s.on('close', cleanup).on('finish', cleanup).on('end', cleanup)
-
-    s.on('heartbeat', function(heartbeat) {
-      var p = self.peers[heartbeat.from]
-      if (p && heartbeat.seq >= p.seq) p.heartbeat = self.heartbeat+1
-    })
-
-    s.on('change', function(change) {
-      self._update(change, s)
-    })
-
-    // index seqs
-    var seqs = handshake.peers.reduce(function(result, peer) {
-      result[peer.id] = peer.seq
-      return result
-    }, {})
-
-    // send local changes to the remote
-    for (var i = 0; i < self.changes.length; i++) {
-      var change = self.changes[i]
-      var seq = seqs[change.from] || 0
-      if (change.seq > seq) s.change(change)
-    }
-
-    self.streams.push(s)
-    heartbeat()
+  s.sync({
+    peers: toArray(this._peers)
   })
 
-  s.handshake({
-    from: this.id,
-    peers: toArray(this.peers)
+  s.once('sync', function(sync) {
+    var seqs = sync.peers.reduce(addSeq, {})
+    var peers = toArray(self._peers)
+
+    var cleanup = once(function() {
+      var i = self._streams.indexOf(s)
+      if (i > -1) self._streams.splice(i, 1)
+    })
+
+    for (var i = 0; i < peers.length; i++) {
+      var p = peers[i]
+      var seq = seqs[p.id] || 0
+
+      for (var j = 0; j < p.changes.length; j++) {
+        if (p.changes[j].seq > seq) s.change(p.changes[j])
+      }
+    }
+
+    s.on('finish', cleanup).on('end', cleanup).on('close', cleanup)
+
+    s.on('change', function(change) {
+      self._update(s, change)
+    })
+
+    self._streams.push(s)
+    self.heartbeat() // we send a heartbeat to indicate that we are alive
   })
 
   return s
@@ -203,19 +230,19 @@ module.exports = Mortable
 
 if (require.main !== module) return
 
-var m1 = Mortable('peer1')
-var m2 = Mortable('peer2')
+var m = Mortable()
+var s = Mortable()
 
-m1.push('hi', 'world')
-m1.push('hi', 'der der')
+m.push('hello', 'world')
+m.push('hello', 'verden')
 
-var s1 = m1.createStream()
-var s2 = m2.createStream()
+var ms = m.createStream()
 
-s1.pipe(s2).pipe(s1)
+ms.pipe(s.createStream()).pipe(ms)
 
 setImmediate(function() {
-  console.log(m2.list('hi'))
-  m1.destroy()
-  console.log(m2.list('hi'))
-}, 100)
+  console.log(s.list())
+  console.log(s.list('hello'))
+})
+
+//console.log(m.has('hello1'), m.list('hello1'))
